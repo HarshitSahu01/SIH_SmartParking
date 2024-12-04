df8b086bdc2420553813b20566ebebd1d0bf7b6b
HarshitSahu01 <sahuharshit2019@gmail.com>
2024-12-04 15:17:15 +0530

    new file:   model/__pycache__/imageProcess.cpython-312.pyc 	modified:   server/__pycache__/urls.cpython-312.pyc 	modified:   server/__pycache__/views.cpython-312.pyc 	modified:   server/urls.py 	modified:   server/views.py 	new file:   yolov8s.pt

--- backend/server/views.py
+++ backend/server/views.py
@@ -7,6 +7,10 @@ from django.http import HttpResponse
 from server.models import *
 from geopy.distance import geodesic
 
+print('Loading model')
+from model.imageProcess import detect_parking_spots_from_image
+print('Model loaded')
+
 RADIUS = 2000 # in metres
 
 def index(request):
@@ -50,6 +54,20 @@ def getCords(request):
             }
         })
     return JsonResponse({'data': data})
+
+
+def testView(request):
+    
+    image_path = 'model/image.png'  # Input image file
+    car_spots_file = 'model/cars.txt'  # Car parking areas file
+    bike_spots_file = 'model/image.txt'  # Motorcycle parking areas file
+
+    try:
+        empty_cars, empty_bikes = detect_parking_spots_from_image(image_path, car_spots_file, bike_spots_file, 'model/coco.txt')
+        print(f"Free car spots: {empty_cars}, Free bike spots: {empty_bikes}")
+    except ValueError as e:
+        print(f"Error: {e}")
+    return JsonResponse({'message': 'Hello World!','data':empty_cars})
 
 '''
 
