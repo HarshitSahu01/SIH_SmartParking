9acf77f7866afc49128200007a7b3db8bef19a85
HarshitSahu01 <sahuharshit2019@gmail.com>
2024-12-04 16:25:21 +0530

    new file:   frontend/src/Components/BoxConfirmCanvas.jsx 	modified:   frontend/src/Pages/TestingLandingPage.jsx 	new file:   frontend/src/assets/sampleParking.png 	deleted:    yolov8s.pt 	deleted:    yolov8s.pt.6da7c6de4e25468cbdfb075ebea401a5.partial

---
+++ frontend/src/Components/BoxConfirmCanvas.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BoxConfirmCanvas() {
+    const path = 'assets/sampleParking.png'
+  return (
+    <div>
+      <img src={path} alt="meow sena" />
+    </div>
+  )
+}
