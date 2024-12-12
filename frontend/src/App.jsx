c8cd66f551ed37bb25a76046add7cb237a0da29f
moliagrawal <159398043+moliagrawal@users.noreply.github.com>
2024-12-12 17:23:15 +0530

    modified:   src/App.jsx 	deleted:    src/Pages/About.jsx 	modified:   src/Pages/AsaWhat.jsx 	modified:   src/Pages/Book.jsx 	modified:   src/Pages/Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import About from './Pages/About';
 import DropPin from './Pages/DropPin';
 import Map from './Pages/Map';
 import Swiper from './Pages/testSwiper';
@@ -38,7 +37,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/maps" element={<Map />} />
         <Route path="/drop-pin" element={<DropPin />} />
         <Route path="/test" element={<Swiper />} />
